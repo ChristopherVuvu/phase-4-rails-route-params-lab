4733162a90ee9339e2433e4fd1cb54beb3f9893e
ChristopherVuvu <vuvumumo93@gmail.com>
2023-06-23 17:21:27 +0300

    added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  #use resource to fetch individual student data
+  resources :students, only: [:index, :show]
+
+  #generate the other get requests i.e index, grades and highest-grade
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,29 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe Student, type: :model do
-  let!(:student) { Student.new(first_name: 'Dwayne', last_name: 'Johnson', grade: 99) }
+RSpec.describe "Students", type: :request do
+  let!(:student1) { Student.create(first_name: "John", last_name: "Mutuku", grade: 79) }
+  let!(:student2) { Student.create(first_name: "Jane", last_name: "Kamau", grade: 92) }
 
-  describe '#first_name' do
-    it 'returns the student\'s first name' do
-      expect(student.first_name).to eq('Dwayne')
+  #get the first student using http status from json
+  describe "GET /students/:id" do
+    it "returns information on the first student" do
+      get "/students/#{student1.id}"
+      expect(response).to have_http_status(200)
+      expect(response.body).to eq(student1.to_json)
+    end
+
+    #get the second student using http status from json
+    it "returns information on the second student" do
+      get "/students/#{student2.id}"
+      expect(response).to have_http_status(200)
+      expect(response.body).to eq(student2.to_json)
     end
   end
 
-  describe '#last_name' do
-    it 'returns the student\'s last name' do
-      expect(student.last_name).to eq('Johnson')
-    end
-  end
-
-  describe '#grade' do
-    it 'returns the student\'s grade' do
-      expect(student.grade).to eq(99)
-    end
-  end
-
-  describe '#to_s' do
-    it 'has a #to_s method that returns the student\'s full name' do
-      expect(student.to_s).to eq('Dwayne Johnson')
+  #get the all students using http status from json
+  describe "GET /students" do
+    it "returns an array of all students" do
+      get "/students"
+      expect(response).to have_http_status(200)
+      expect(response.body).to eq([student1, student2].to_json)
     end
   end
 end
+
+
+
+
+
+
+
